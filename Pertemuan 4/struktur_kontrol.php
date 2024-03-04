514b4ebd30b292ecb4d8979512de49ceba0f4a77
[Yunika Putri] <[yunikaputridwia@gmail.com]>
2024-03-05 00:35:55 +0700

    menghitung dan mencetak total skor ujian dari array

--- Pertemuan 4/struktur_kontrol.php
+++ Pertemuan 4/struktur_kontrol.php
@@ -38,4 +38,15 @@ for ($i = 1; $i <= $jumlahLahan; $i++) {
 }
 
 echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
+echo "<br><br>";
+
+// Menghitung dan mencetak total skor ujian dari array
+$skorUjian = [85, 92, 78, 96, 88];
+$totalSkor = 0;
+
+foreach ($skorUjian as $skor) {
+    $totalSkor += $skor;
+}
+
+echo "Total skor ujian adalah: $totalSkor";
 ?>
