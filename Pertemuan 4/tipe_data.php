b0cce5914cab1c395d64b36181d33bf7fd3d9119
[Yunika Putri] <[yunikaputridwia@gmail.com]>
2024-03-04 09:25:33 +0700

    commit

--- Pertemuan 4/tipe_data.php
+++ Pertemuan 4/tipe_data.php
@@ -12,4 +12,17 @@ echo "Variabel d: {$d} <br>";
 echo "Variabel e: {$e} <br>";
 
 var_dump($e);
+
+$nilaiMatematika = 5.1;
+$nilaiIPA = 6.7;
+$nilaiBahasaIndonesia = 9.3;
+
+$rataRata = ($nilaiMatematika + $nilaiIPA + $nilaiBahasaIndonesia) /3;
+
+echo "Matematika: {$nilaiMatematka} <br>";
+echo "IPA: {$nilaiIPA} <br>";
+echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
+echo "Rata-rata: {$rataRata} <br>";
+
+var_dump($rataRata);
 ?>
