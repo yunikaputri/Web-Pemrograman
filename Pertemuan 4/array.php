7f19ebfd47a861fc36b0e0b77bd3aee5db24f65a
[Yunika Putri] <[yunikaputridwia@gmail.com]>
2024-03-05 21:45:23 +0700

    memfilter dan mecetak daftar karyawan yang memiliki pengalaman kerja lebih dari 5 tahun

--- Pertemuan 4/array.php
+++ Pertemuan 4/array.php
@@ -11,4 +11,24 @@ foreach ($nilaiSiswa as $nilai) {
 }
 
 echo "Daftar nilai siswa yang lulus: " . implode(', ', $nilaiLulus);
+echo "<br><br>";
+
+// Memfilter dan mecetak daftar karyawan yang memiliki pengalaman kerja lebih dari 5 tahun
+$daftarKaryawan = [
+    ['Alice', 7],
+    ['Bob', 3],
+    ['Charlie', 9],
+    ['David', 5],
+    ['Eva', 6],
+];
+
+$karyawanPengalamanLimaTahun = [];
+
+foreach ($daftarKaryawan as $karyawan) {
+    if ($karyawan[1] > 5) {
+        $karyawanPengalamanLimaTahun[] = $karyawan[0];
+    }
+}
+
+echo "Daftar karyawan dengan pengalaman kerja lebih dari 5 tahun: " . implode(', ', $karyawanPengalamanLimaTahun);
 ?>
