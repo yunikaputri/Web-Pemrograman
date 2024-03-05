1530fffc411c56e7cd26dbea5ac62c212e0bbac7
[Yunika Putri] <[yunikaputridwia@gmail.com]>
2024-03-05 21:31:21 +0700

    memfilter dan mecetak nilai siswa yang lebih besar dari atau sama dengan 70

---
+++ Pertemuan 4/array.php
@@ -0,0 +1,14 @@
+<?php
+// Memfilter dan mencetak nilai siswa yang lebih besar dari atau sama dengan 70
+$nilaiSiswa = [85, 92, 78, 64, 90, 55, 88, 79, 70, 96];
+
+$nilaiLulus = [];
+
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai >= 70) {
+        $nilaiLulus[] = $nilai;
+    }
+}
+
+echo "Daftar nilai siswa yang lulus: " . implode(', ', $nilaiLulus);
+?>
