40778ab77c498e37f1f76537f85e7fb5c824e547
[Yunika Putri] <[yunikaputridwia@gmail.com]>
2024-03-07 17:01:21 +0700

    menambahkan data orang pda array mutidimensi

--- Pertemuan 4/arrayMultidimensi.php
+++ Pertemuan 4/arrayMultidimensi.php
@@ -27,11 +27,32 @@ $students = [
         "nim" => 2242520192,
         "jurusan" =>  'Akuntasi',
         'email' =>'lutfaniramzi@gmail.com'
+    ],
+    [
+        "image" => "img/abim.jpg",
+        "nama" => "Bimantara Dwi Cahyo",
+        "nim" => 2241720188,
+        "jurusan" =>  'Teknologi Informasi',
+        'email' =>'bmntrdwicahyo@gmail.com'
+    ],
+    [
+        "image" => "img/fajar.jpg",
+        "nama" => "Muhammad Ilham Fajar",
+        "nim" => 224728474,
+        "jurusan" =>  'Teknik Elektro',
+        'email' =>'mfjrilaham@gmail.com'
+    ],
+    [
+        "image" => "img/haqi.jpg",
+        "nama" => "Muhammad Dhiyaulhaq",
+        "nim" => 224792373,
+        "jurusan" =>  'Teknik Listrik',
+        'email' =>'mdhayulhaqq@gmail.com'
     ]
 ];
 
 foreach ($students as $student) {
-    echo "<div class='student-container'>"; // Menambahkan kelas student-container
+    echo "<div class='student-container'>"; 
     echo "<img src='" . $student['image'] . "' alt='" . $student['nama'] . "' width='200' height='200'><br>";
     echo "• Nama: " . $student['nama'] . "<br>";
     echo "• NIM: " . $student['nim'] . "<br>";
