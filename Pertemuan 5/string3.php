32a7686b362875ae294696ca9903cd6f9fa884cc
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-18 22:12:45 +0700

    membalik String lagi

--- Pertemuan 5/string3.php
+++ Pertemuan 5/string3.php
@@ -3,4 +3,16 @@
 // membalik string  menggunakan perintah strrev().
 $pesan = "Saya arek Probolinggo";
 echo strrev($pesan) . "<br>";
+
+
+// membalik String lagi
+$pesan = "Saya arek Probolinggo";
+# ubah variabel $pesan menjadi array dengan perintah explode
+$pesanPerKata = explode(" ", $pesan);
+# ubah setiap kata dalam array menjadi kebalikannya
+$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+# gabungkan kembali array menjadi string
+$pesan = implode(" ", $pesanPerKata);
+
+echo $pesan . "<br>";
 ?>
