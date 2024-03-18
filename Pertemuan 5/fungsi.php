cce460ee3b3f3eb870ab81ada9960911dc7f5004
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-18 20:53:33 +0700

    fungsi dengan parameter

--- Pertemuan 5/fungsi.php
+++ Pertemuan 5/fungsi.php
@@ -1,12 +1,30 @@
 <?php
+// //merupakan fungsi tanpa parameter
+// function perkenalan(){
+//     echo "Assalamualaikum, ";
+//     echo "Perkenalkan, nama saya Yunika<br/>";
+//     echo "Senang berkenalan dengan Anda<br/>";
+// }
 
-//merupakan fungsi tanpa parameter
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Nabila<br/>";
+// //memanggil fungsi yang sudah dibuat
+// perkenalan();
+// perkenalan();
+
+// fungsi dengan parameter
+function perkenalan($nama, $salam){
+    echo $salam. ", ";
+    echo "Perkenalkan, nama saya " .$nama. "<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
 
-//memanggil fungsi yang sudah dibuat
-perkenalan();
+// memanggil fungsi yang sudah dibuat
+perkenalan("Yunika " , "Hallo");
+
+echo "<hr>";
+
+$saya = "Yunika";
+$ucapanSalam = "Selamat pagi";
+
+// memanggil lagi
+perkenalan($saya, $ucapanSalam);
 ?>
