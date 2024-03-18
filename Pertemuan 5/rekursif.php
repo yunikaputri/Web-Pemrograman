8aae779a1340f673b92fdd7bc104f27e56144904
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-18 21:45:39 +0700

    panggil diri sendiri selama $indeks <<= $jumlah

--- Pertemuan 5/rekursif.php
+++ Pertemuan 5/rekursif.php
@@ -9,8 +9,18 @@
 // tampilkanHaloDunia();
 
 
-// rekursif menampilkan angka 1-25
-for($i=1; $i <= 25; $i++){
-    echo "Perulangan ke-{$i} <br>";
+// // rekursif menampilkan angka 1-25
+// for($i=1; $i <= 25; $i++){
+//     echo "Perulangan ke-{$i} <br>";
+// }
+
+function tampilkanAngka (int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke-{$indeks} <br>";
+    
+//panggil diri sendiri selama $indeks <<= $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    } 
 }
+tampilkanAngka(20);
 ?>
