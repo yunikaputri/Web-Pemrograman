bc282e8462afa7c83c7b9b33364443a92bbb855d
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-19 20:48:58 +0700

    fungsi tanpa parameter

---
+++ Pertemuan 6/praktik_php/fungsi.php
@@ -0,0 +1,12 @@
+<?php
+//merupakan fungsi tanpa parameter
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Yunika<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+//memanggil fungsi yang sudah dibuat
+perkenalan();
+perkenalan();
+?>
