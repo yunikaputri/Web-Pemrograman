15768ab2164f907b8d17ee3550216566c47b2269
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-19 22:15:18 +0700

    enampilkan tanggal dalam beberapa format yang berbeda menggunakan fungsi date() PHP

---
+++ Pertemuan 6/date.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <h3>Date</h3>
+        <?php
+        // menampilkan tanggal dalam beberapa format yang berbeda menggunakan fungsi date() PHP
+            echo "Today is " . date("Y/m/d") . "<br>";
+            echo "Today is " . date("Y.m.d") . "<br>";
+            echo "Today is " . date("Y-m-d") . "<br>";
+            echo "Today is " . date("1");
+        ?>
+    </body>
+</html>
