e5c4384e06ac9c36e0877cba9cf5cb8ffc8c8883
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-19 20:55:09 +0700

    fungsi dengan parameter default

--- Pertemuan 6/praktik_php/fungsi.php
+++ Pertemuan 6/praktik_php/fungsi.php
@@ -11,21 +11,39 @@
 // perkenalan();
 
 
-// fungsi dengan parameter
-function perkenalan($nama, $salam){
-    echo $salam. ", ";
-    echo "Perkenalkan, nama saya " .$nama. "<br/>";
+// // fungsi dengan parameter
+// function perkenalan($nama, $salam){
+//     echo $salam. ", ";
+//     echo "Perkenalkan, nama saya " .$nama. "<br/>";
+//     echo "Senang berkenalan dengan Anda<br/>";
+// }
+
+// // memanggil fungsi yang sudah dibuat
+// perkenalan("Yunika " , "Hallo");
+
+// echo "<hr>";
+
+// $saya = "Yunika";
+// $ucapanSalam = "Selamat pagi";
+
+// // memanggil lagi
+// perkenalan($saya, $ucapanSalam);
+
+
+// fungsi dengan parameter default
+// membuat fungsi
+function perkenalan ($nama, $salam = "Assalamualaikum") {
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
 
-// memanggil fungsi yang sudah dibuat
-perkenalan("Yunika " , "Hallo");
-
+//memanggil fungsi yang sudah dibuat perkenalan 
+perkenalan("Yunika","Hallo");
 echo "<hr>";
-
-$saya = "Yunika";
+$saya = "Elok";
 $ucapanSalam = "Selamat pagi";
 
-// memanggil lagi
-perkenalan($saya, $ucapanSalam);
+//memanggil lagi tanpa mengisi parameter salam perkenalan 
+perkenalan($saya);
 ?>
