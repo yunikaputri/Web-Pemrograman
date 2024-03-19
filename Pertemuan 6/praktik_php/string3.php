abb8d18c7e2cbd8adceff3ac0c2c226f1e43723b
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-19 21:25:10 +0700

    membalik string perkata

--- Pertemuan 6/praktik_php/string3.php
+++ Pertemuan 6/praktik_php/string3.php
@@ -1,5 +1,17 @@
 <?php
-// membalik string  menggunakan perintah strrev().
+// // membalik string  menggunakan perintah strrev().
+// $pesan = "Saya arek Probolinggo";
+// echo strrev($pesan) . "<br>";
+
+
+// membalik string perkata
 $pesan = "Saya arek Probolinggo";
-echo strrev($pesan) . "<br>";
+# ubah variabel $pesan menjadi array dengan perintah explode
+$pesanPerKata = explode(" ", $pesan);
+# ubah setiap kata dalam array menjadi kebalikannya
+$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+# gabungkan kembali array menjadi string
+$pesan = implode(" ", $pesanPerKata);
+
+echo $pesan . "<br>";
 ?>
