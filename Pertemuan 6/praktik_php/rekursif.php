0c1f51d5f69801a5ac97afd5cd5e74950a14ca47
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-19 21:09:04 +0700

    menggunakan konsep fungsi rekursif

--- Pertemuan 6/praktik_php/rekursif.php
+++ Pertemuan 6/praktik_php/rekursif.php
@@ -9,8 +9,20 @@
 // tampilkanHaloDunia();
 
 
-// rekursif menampilkan angka 1-25
-for($i=1; $i <= 25; $i++){
-    echo "Perulangan ke-{$i} <br>";
+// // rekursif menampilkan angka 1-25
+// for($i=1; $i <= 25; $i++){
+//     echo "Perulangan ke-{$i} <br>";
+// }
+
+
+// menggunakan konsep fungsi rekursif 
+function tampilkanAngka (int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke-{$indeks} <br>";
+    
+//panggil diri sendiri selama $indeks <<= $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    } 
 }
+tampilkanAngka(20);
 ?>
