ba1a8adf53d4ecbf37574faffac15b67b0f92489
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-19 19:36:04 +0700

    mendefinisikan array $ListDosen yang berisi daftar nama dosen

---
+++ array_1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <h2>Array Terindeks</h2>
+    <?php
+    // mendefinisikan array $ListDosen yang berisi daftar nama dosen
+        $ListDosen=["Elok Nur Hamdana","Unggul Pamenang","Bagas Nugraha"];
+        echo $ListDosen[2] . "<br>";
+        echo $ListDosen[0] . "<br>";
+        echo $ListDosen[1] . "<br>";
+    ?>
+</body>
+</html>
