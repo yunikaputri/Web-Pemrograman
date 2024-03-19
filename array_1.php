1258e79826fced68374c72c17839163d6f00ceb4
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-19 19:42:33 +0700

    menampilakn array menggunakan indeks perulangan

--- array_1.php
+++ array_1.php
@@ -6,10 +6,16 @@
     <h2>Array Terindeks</h2>
     <?php
     // mendefinisikan array $ListDosen yang berisi daftar nama dosen
-        $ListDosen=["Elok Nur Hamdana","Unggul Pamenang","Bagas Nugraha"];
-        echo $ListDosen[2] . "<br>";
-        echo $ListDosen[0] . "<br>";
-        echo $ListDosen[1] . "<br>";
+        // $ListDosen=["Elok Nur Hamdana","Unggul Pamenang","Bagas Nugraha"];
+        // echo $ListDosen[2] . "<br>";
+        // echo $ListDosen[0] . "<br>";
+        // echo $ListDosen[1] . "<br>";
+
+        // menampilakn array menggunakan indeks perulangan
+        $ListDosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+        foreach ($ListDosen as $dosen) {
+            echo $dosen . "<br>";
+        }
     ?>
 </body>
 </html>
