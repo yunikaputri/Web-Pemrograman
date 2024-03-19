e7c39f7c75f351fa6f802b554c7b57c678416eac
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-19 22:33:57 +0700

    penggunaan variabel server yang menyediakan berbagai macam informasi tentang request yang ditangkap oleh server

--- Pertemuan 6/variable_server.php
+++ Pertemuan 6/variable_server.php
@@ -1,4 +1,20 @@
 <?php
-// mengonversi variabel $_SERVER menjadi format JSON dan mencetaknya
-echo json_encode($_SERVER);
+// // mengonversi variabel $_SERVER menjadi format JSON dan mencetaknya
+// echo json_encode($_SERVER);
+
+
+/* penggunaan variabel server yang menyediakan berbagai macam informasi 
+tentang request yang ditangkap oleh server */
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER']; //Hasil Error dikarenakan datanya kosong/tidak ditemukan
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+
 ?>
