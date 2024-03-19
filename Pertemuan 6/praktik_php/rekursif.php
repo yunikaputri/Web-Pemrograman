04641faf1e91c8f062894e0b07d65e359bf1c0b4
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-19 21:07:10 +0700

    rekursif menampilkan angka 1-25

--- Pertemuan 6/praktik_php/rekursif.php
+++ Pertemuan 6/praktik_php/rekursif.php
@@ -1,10 +1,16 @@
 <?php
-// rekrusif menampilakn teks
-function tampilkanHaloDunia(){
-    echo "Halo dunia! <br>";
+// // rekrusif menampilakn teks
+// function tampilkanHaloDunia(){
+//     echo "Halo dunia! <br>";
 
-    tampilkanHaloDunia();
+//     tampilkanHaloDunia();
+// }
+
+// tampilkanHaloDunia();
+
+
+// rekursif menampilkan angka 1-25
+for($i=1; $i <= 25; $i++){
+    echo "Perulangan ke-{$i} <br>";
 }
-
-tampilkanHaloDunia();
 ?>
