7efde0854cf90ed324b7b4c35520a369ff4dcd22
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-20 23:48:11 +0700

    memeriksa apakah input adalah email yang valid

--- Pertemuan 7/html_aman.php
+++ Pertemuan 7/html_aman.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        Input: <input type="text" name="Yunika">
+        Input: <input type="text" name="input">
         <input type="submit" value="Submit">
     </form>
 
@@ -15,10 +15,21 @@
     // Memeriksa apakah form sudah disubmit menggunakan metode POST
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Mengambil nilai input dari form dan menghindari serangan XSS
-        $input = $_POST['Yunika'];
-        $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-        // Menampilkan input yang dimasukkan
-        echo "Input yang dimasukkan: " . $input;
+        if(isset($_POST['input'])){
+            $input = htmlspecialchars($_POST['input'], ENT_QUOTES, 'UTF-8');
+            // Menampilkan input yang dimasukkan
+            echo "Input yang dimasukkan: " . $input;
+        }
+    }
+
+    // Memeriksa apakah input adalah email yang valid
+    if(isset($_POST['email'])){
+        $email = $_POST['email'];
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Lanjutkan dengan pengolahan email yang aman
+        } else {
+            // Tangani input yang tidak valid
+        }
     }
     ?>
 </body>
