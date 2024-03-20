a17fda6414ee265e1468ec59b6a1932ec85d8948
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-20 22:57:28 +0700

    memeriksa apakah array $myArray kosong atau tidak terdefinisikan

---
+++ Pertemuan 7/empty.php
@@ -0,0 +1,9 @@
+<?php 
+// Memeriksa apakah array $myArray kosong atau tidak terdefinisikan
+$myArray = array(); // Array kosong
+if (empty($myArray)) {
+    echo "Array tidak terdefinisikan atau kosong.";
+} else {
+    echo "Array tidak terdefinisikan atau kosong.";
+}
+?>
