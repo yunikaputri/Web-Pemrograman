7c266560ce50506e4a3bc7de03818e09dec1fa05
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-20 23:56:04 +0700

    mencocokkan satu atau lebih digit dalam teks dan menampilkan hasil cocokan

--- Pertemuan 7/regex.php
+++ Pertemuan 7/regex.php
@@ -7,4 +7,13 @@ if (preg_match($pattern, $text) ) {
 } else {
     echo "Tidak ada huruf kecil!";
 }
+
+// Mencocokkan satu atau lebih digit dalam teks dan menampilkan hasil cocokan
+$pattern = '/[0-9]+/'; // Cocokkan satu atau lebih digit.
+$text = 'There are 123 apples.';
+if (preg_match($pattern, $text, $matches) ) {
+    echo "<br/>Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
 ?>
