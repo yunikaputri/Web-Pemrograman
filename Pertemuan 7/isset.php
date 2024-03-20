6e6cf7059a86de860a2379b4b3f05fa59c049048
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-20 22:51:31 +0700

    memeriksa apakah kunci "nama" telah didefinisikan dalam array $data sebelum mencoba mengakses nilainya

--- Pertemuan 7/isset.php
+++ Pertemuan 7/isset.php
@@ -4,6 +4,14 @@ $umur;
 if (isset($umur) && $umur >= 18) {
     echo "Anda sudah dewasa. ";
 } else {
-    echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.";
+    echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.<br/>";
+}
+
+// Memeriksa apakah kunci "nama" telah didefinisikan dalam array $data sebelum mencoba mengakses nilainya
+$data = array("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array";
 }
 ?>
