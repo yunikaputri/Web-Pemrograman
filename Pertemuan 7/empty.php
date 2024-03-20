9c04346654c75c7fffaa65606ef5e3b645e3e342
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-20 23:08:47 +0700

    memeriksa apakah variabel $nonExistentVar tidak terdefinisikan atau kosong

--- Pertemuan 7/empty.php
+++ Pertemuan 7/empty.php
@@ -4,6 +4,13 @@ $myArray = array(); // Array kosong
 if (empty($myArray)) {
     echo "Array tidak terdefinisikan atau kosong.";
 } else {
-    echo "Array tidak terdefinisikan atau kosong.";
+    echo "Array terdefinisikan dan tidak kosong.";
+}
+
+// Memeriksa apakah variabel $nonExistentVar tidak terdefinisikan atau kosong
+if (empty($nonExistentVar)) {
+    echo "<br/>Array tidak terdefinisikan atau kosong.";
+} else {
+    echo "Array terdefinisikan dan tidak kosong.";
 }
 ?>
