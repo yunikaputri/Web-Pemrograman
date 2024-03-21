0cddd602f2253a5772b796ebafea47e63ed46339
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-21 12:53:22 +0700

    Praktikum bagian 4 memodifikasi script yang sudah di buat pada Praktikum 3 yaitu form_upload_ajax.php

--- Pertemuan 8/form_upload_ajax.php
+++ Pertemuan 8/form_upload_ajax.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <head>
     <title>Unggah File Dokumen</title>
 </head>
-<body>
+<body> -->
     <!-- Menyiapkan formulir HTML untuk mengunggah file dokumen dengan dukungan JavaScript -->
-    <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
+    <!-- <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
         <input type="file" name="file" id="file">
         <input type="submit" name="submit" value="Unggah">
     </form>
@@ -14,4 +14,34 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="upload.js"></script>
 </body>
+</html> -->
+
+
+<!-- Mengatur tampilan formulir unggah file dokumen dengan desain kustom dan menggunakan JavaScript untuk mengelola perilaku tombol unggah -->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="upload.css">
+    <title>Unggah File Dokumen</title>
+</head>
+
+<body>
+    <div class="upload-form-container">
+        <h2>Unggah File Dokumen</h2>
+        <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
+            <div class="file-input-container">
+                <input type="file" name="file" id="file" class="file-input">
+                <label for="file" class="file-label">Pilih File</label>
+            </div>
+            <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>Unggah</button>
+        </form>
+
+        <div id="status" class="upload-status"></div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="upload.js"></script>
+</body>
+
 </html>
