fe5339682b7e1bea7775adef74673d08a489ef50
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-21 11:42:47 +0700

    mengelola unggahan file dari form_multiupload.php

---
+++ Pertemuan 8/proses_upload.php
@@ -0,0 +1,28 @@
+<?php
+// Mengelola unggahan file dari form_multiupload.php
+// Lokasi penyimpanan file yang diunggah
+$targetDirectory = "C:/xampp/htdocs/dasarWeb/Pertemuan 8/";
+
+// Periksa apakah direktori penyimpanan ada, jika tidak maka buat
+if (!file_exists($targetDirectory)) {
+    mkdir($targetDirectory, 0777, true);
+}
+
+if ($_FILES['files']['name'][0]) {
+    $totalFiles = count($_FILES['files']['name']);
+
+    // Loop melalui semua file yang diunggah
+    for ($i = 0; $i < $totalFiles; $i++) {
+        $fileName = $_FILES['files']['name'][$i];
+        $targetFile = $targetDirectory . $fileName;
+
+        //Pindahkan file yang diunggah ke direktori penyimpanan
+        if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
+            echo "File $fileName berhasil diunggah. <br>";
+        } else {
+            echo "Gagal mengunggah file $fileName. <br>";
+        }
+    }
+} else {
+    echo "Tidak ada file yang diunggah.";
+}
