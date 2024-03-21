a007e7ec7bc0073ac0e10ad780a11c6319d9d54c
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-22 01:53:00 +0700

    mengambil data pengguna dari formulir login dan melakukan pengolahan data untuk proses autentikasi

---
+++ Pertemuan 9/praktik_php/loginMultiProses.php
@@ -0,0 +1,25 @@
+<?php
+// Mengambil data pengguna dari formulir login dan melakukan pengolahan data untuk proses autentikasi
+    include "koneksi.php";
+    $username = $_POST['username'];
+    $password = md5 ($_POST['password']);
+
+    $query = "SELECT * FROM user WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($connect, $query);
+    $row = mysqli_fetch_assoc ($result);
+
+    if ($row['level'] == 1) {
+        echo "Anda berhasil login. silahkan menuju "; ?>
+        <a href="homeAdmin.html">Halaman HOME</a>
+    <?php
+    } else if ($row['level'] == 2) {
+        echo "Anda gagal login. silahkan menuju "; ?>
+        <a href="homeGuest.html">Halaman Home</a>
+    <?php
+    } else {
+        echo "Anda gagal login. silahkan "; ?>
+        <a href="loginForm.html">Login kembali</a>
+        <?php
+        echo mysqli_error($connect) ;
+    }
+?>
