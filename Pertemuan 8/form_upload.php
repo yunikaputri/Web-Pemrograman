aa90440a6436a7dc07114e5b073898bf7fb3a1fe
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-21 11:29:47 +0700

    mengunggah document ke server menggunakan metode POST dan enctype multipart/form-data

--- Pertemuan 8/form_upload.php
+++ Pertemuan 8/form_upload.php
@@ -5,8 +5,14 @@
 </head>
 <body>
     <!-- Mengunggah file ke server menggunakan metode POST dan enctype multipart/form-data -->
-    <form action="upload.php" method="post" enctype="multipart/form-data">
+    <!-- <form action="upload.php" method="post" enctype="multipart/form-data">
         <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload File" name="submit">
+    </form> -->
+
+    <!-- Mengunggah document ke server menggunakan metode POST dan enctype multipart/form-data -->
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="documentToUpload" id="documentToUpload">
         <input type="submit" value="Upload File" name="submit">
     </form>
 </body>
--- Pertemuan 8/upload.php
+++ Pertemuan 8/upload.php
@@ -1,68 +1,93 @@
 <?php
+// // Mengelola unggahan file dari form_upload.php
+// if (isset($_POST["submit"])) {
+//     $targetDirectory = "C:/xampp/htdocs/dasarWeb/Pertemuan 8/"; // Ganti dengan path yang sesuai
+//     $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
+//     $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+//     // Melakukan validasi tipe file yang diunggah
+//     $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+
+//     // Melakukan validasi ukuran file yang diunggah
+//     $maxFileSize = 5 * 1024 * 1024; // 5 MB
+    
+//     if (in_array($fileType, $allowedExtensions) && $_FILES["fileToUpload"]["size"] <= $maxFileSize) {
+//         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
+//             echo "File berhasil diunggah.";
+
+//             // Membuat thumbnail
+//             if (function_exists('createThumbnail')) {
+//                 createThumbnail($targetFile, "thumbnails/" . basename($targetFile), 200);
+//                 echo "<br><br>";
+//                 echo '<img src="thumbnails/' . basename($targetFile) . '" alt="Thumbnail">';
+//             } else {
+//                 echo "GD Library tidak tersedia, thumbnail tidak dapat dibuat.";
+//             }
+//         } else {
+//             echo "Gagal mengunggah file.";
+//         }
+//     } else {
+//         echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
+//     }
+// }
+
+// function createThumbnail($sourceFile, $targetFile, $maxWidth)
+// {
+//     list($sourceWidth, $sourceHeight, $sourceType) = getimagesize($sourceFile);
+
+//     // Menghitung tinggi thumbnail sesuai dengan lebar 200
+//     $maxHeight = ($maxWidth / $sourceWidth) * $sourceHeight;
+
+//     // Membuat gambar thumbnail
+//     $thumb = imagecreatetruecolor($maxWidth, $maxHeight);
+//     if ($sourceType == IMAGETYPE_JPEG) {
+//         $sourceImage = imagecreatefromjpeg($sourceFile);
+//     } elseif ($sourceType == IMAGETYPE_PNG) {
+//         $sourceImage = imagecreatefrompng($sourceFile);
+//     } elseif ($sourceType == IMAGETYPE_GIF) {
+//         $sourceImage = imagecreatefromgif($sourceFile);
+//     } else {
+//         return false; // Tidak dapat membuat thumbnail untuk tipe file yang tidak didukung
+//     }
+
+//     // Menyalin dan mengubah ukuran gambar sumber ke thumbnail
+//     imagecopyresampled($thumb, $sourceImage, 0, 0, 0, 0, $maxWidth, $maxHeight, $sourceWidth, $sourceHeight);
+
+//     // Menyimpan thumbnail sebagai file
+//     if ($sourceType == IMAGETYPE_JPEG) {
+//         imagejpeg($thumb, $targetFile);
+//     } elseif ($sourceType == IMAGETYPE_PNG) {
+//         imagepng($thumb, $targetFile);
+//     } elseif ($sourceType == IMAGETYPE_GIF) {
+//         imagegif($thumb, $targetFile);
+//     }
+
+//     imagedestroy($thumb);
+//     imagedestroy($sourceImage);
+// }
+
+
+
 // Mengelola unggahan file dari form_upload.php
 if (isset($_POST["submit"])) {
-    $targetDirectory = "C:/xampp/htdocs/dasarWeb/Pertemuan 8/"; // Ganti dengan path yang sesuai
-    $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
-    $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+    $targetDirectory = "C:/xampp/htdocs/dasarWeb/Pertemuan 8/"; //Direktori tujuan untuk menyimpan file
+    $targetFile = $targetDirectory . basename($_FILES["documentToUpload"]["name"]);
+    $documentFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
     // Melakukan validasi tipe file yang diunggah
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
 
     // Melakukan validasi ukuran file yang diunggah
-    $maxFileSize = 5 * 1024 * 1024; // 5 MB
-    
-    if (in_array($fileType, $allowedExtensions) && $_FILES["fileToUpload"]["size"] <= $maxFileSize) {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
-            echo "File berhasil diunggah.";
+    $maxFileSize = 10 * 1024 * 1024; //10 MB
 
-            // Membuat thumbnail
-            if (function_exists('createThumbnail')) {
-                createThumbnail($targetFile, "thumbnails/" . basename($targetFile), 200);
-                echo "<br><br>";
-                echo '<img src="thumbnails/' . basename($targetFile) . '" alt="Thumbnail">';
-            } else {
-                echo "GD Library tidak tersedia, thumbnail tidak dapat dibuat.";
-            }
+    if (isset($_FILES["documentToUpload"]) && in_array($documentFileType, $allowedExtensions) && $_FILES["documentToUpload"]["size"] <= $maxFileSize) {
+        if (move_uploaded_file($_FILES["documentToUpload"]["tmp_name"], $targetFile)) {
+            echo "Document berhasil diunggah.";
         } else {
             echo "Gagal mengunggah file.";
         }
     } else {
-        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
+        echo "Jenis dokumen tidak valid atau melebihi ukuran maksimum yang diizinkan.";
     }
-}
-
-function createThumbnail($sourceFile, $targetFile, $maxWidth)
-{
-    list($sourceWidth, $sourceHeight, $sourceType) = getimagesize($sourceFile);
-
-    // Menghitung tinggi thumbnail sesuai dengan lebar 200
-    $maxHeight = ($maxWidth / $sourceWidth) * $sourceHeight;
-
-    // Membuat gambar thumbnail
-    $thumb = imagecreatetruecolor($maxWidth, $maxHeight);
-    if ($sourceType == IMAGETYPE_JPEG) {
-        $sourceImage = imagecreatefromjpeg($sourceFile);
-    } elseif ($sourceType == IMAGETYPE_PNG) {
-        $sourceImage = imagecreatefrompng($sourceFile);
-    } elseif ($sourceType == IMAGETYPE_GIF) {
-        $sourceImage = imagecreatefromgif($sourceFile);
-    } else {
-        return false; // Tidak dapat membuat thumbnail untuk tipe file yang tidak didukung
-    }
-
-    // Menyalin dan mengubah ukuran gambar sumber ke thumbnail
-    imagecopyresampled($thumb, $sourceImage, 0, 0, 0, 0, $maxWidth, $maxHeight, $sourceWidth, $sourceHeight);
-
-    // Menyimpan thumbnail sebagai file
-    if ($sourceType == IMAGETYPE_JPEG) {
-        imagejpeg($thumb, $targetFile);
-    } elseif ($sourceType == IMAGETYPE_PNG) {
-        imagepng($thumb, $targetFile);
-    } elseif ($sourceType == IMAGETYPE_GIF) {
-        imagegif($thumb, $targetFile);
-    }
-
-    imagedestroy($thumb);
-    imagedestroy($sourceImage);
 }
 ?>
