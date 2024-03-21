ef7e53481e28c493fc1838720da5116cf631901a
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-21 12:54:49 +0700

    Praktikum bagian 7 memodifikasi script yang sudah di buat pada Praktikum 3 yaitu upload.js

--- Pertemuan 8/upload.js
+++ Pertemuan 8/upload.js
@@ -1,5 +1,37 @@
+// $(document).ready(function() {
+//     // Menangani pengiriman formulir secara asinkron melalui AJAX dan menampilkan status hasilnya
+//     $('#upload-form').submit(function(e) {
+//         e.preventDefault();
+
+//         var formData = new FormData(this);
+
+//         $.ajax({
+//             type: 'POST',
+//             url: 'upload_ajax.php',
+//             data: formData,
+//             cache: false,
+//             contentType: false,
+//             processData: false,
+//             success: function(response) {
+//                 $('#status').html(response);
+//             },
+//             error: function() {
+//                 $('#status').html('Terjadi kesalahan saat mengunggah file.');
+//             }
+//         });
+//     });
+// });
+
+
 $(document).ready(function() {
-    // Menangani pengiriman formulir secara asinkron melalui AJAX dan menampilkan status hasilnya
+    $('#file').change(function() {
+        if (this.files.length > 0) {
+            $('#upload-button').prop('disabled', false).css('opacity', 1);
+        } else {
+            $('#upload-button').prop('disabled', true).css('opacity', 0.5);
+        }
+    });
+
     $('#upload-form').submit(function(e) {
         e.preventDefault();
 
