4c2a04d33e9aa8dd4bd3436c9b1e8325c54d9988
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-21 23:56:51 +0700

    mengecek login pengguna dengan mengambil data dari formulir login, memeriksa dengan database, dan menampilkan pesan keberhasilan atau kegagalan login

---
+++ Pertemuan 9/praktik_php/loginProses.php
@@ -0,0 +1,22 @@
+<?php
+    // Mengecek login pengguna dengan mengambil data dari formulir login, memeriksa dengan database, dan menampilkan pesan keberhasilan atau kegagalan login
+    include "koneksi.php";
+
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+
+    $query = "SELECT * FROM user WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($connect, $query);
+    $cek = mysqli_num_rows($result);
+
+    if ($cek) {
+        echo "Anda berhasil login. Silahkan menuju"; ?>
+        <a href='homeAdmin.html'>Halaman HOME</a>;
+    <?php
+    } else {
+        echo "Anda gagal login. Silahkan"; ?>
+        <a href='loginForm.html'>Login kembali</a>;
+    <?php
+        echo mysqli_error($connect);
+    }
+?>
