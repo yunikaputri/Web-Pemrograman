3950e65a9619c8428cf612fed79abd9e54bb6408
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-23 17:08:39 +0700

    mengatur proses pengubahan data anggota berdasarkan ID dengan nilai-nilai baru

--- Pertemuan 10/crud/proses.php
+++ Pertemuan 10/crud/proses.php
@@ -17,6 +17,21 @@ if ($aksi == 'tambah') {
     } else {
         echo "Gagal menambahkan data: " . mysqli_error($koneksi);
     }
+} else if ($aksi == 'ubah') {
+    // Mengatur proses pengubahan data anggota berdasarkan ID dengan nilai-nilai baru
+    if (isset($_POST['id'])) {
+        $id = $_POST['id'];
+
+        $query = "UPDATE anggora SET nama='$nama', jenis_kelamin='$jenis_kelamin', alamat='$alamat', no_telp='$no_telp' WHERE id = $id";
+        if (mysqli_query($koneksi, $query)) {
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Gagal menghapus data: " . mysqli_error($koneksi);
+        }
+    } else {
+        echo "ID tidak valid.";
+    }
 }
 mysqli_close($koneksi);
 ?>
