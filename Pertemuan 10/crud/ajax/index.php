97393ce183bbb440bfc392660b6abbaba17fabe0
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-23 18:24:55 +0700

    menggunakan Ajax untuk memuat data dari file "data.php" ke dalam elemen dengan kelas "data" pada halaman HTML.

---
+++ Pertemuan 10/crud/ajax/index.php
@@ -0,0 +1,96 @@
+<?php
+include 'auth.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- Menggunakan Ajax untuk memuat data dari file "data.php" ke dalam elemen dengan kelas "data" pada halaman HTML -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Crsf Token -->
+    <meta name="csrf-token" content="<?= $_SESSION['csrf_token'] ?>">
+    <!-- Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <!-- Datatable -->
+    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <title>Data Anggota</title>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-primary">
+        <a href="index.php" class="navbar-brand" style="color: #fff">
+            CRUD Dengan Ajax
+        </a>
+    </nav>
+
+    <div class="container">
+        <h2 align="center" style="margin: 30px;">Data Anggota</h2>
+
+        <form method="post" class="form-data" id="form-data">
+            <div class="row">
+                <div class="col-sm-9">
+                    <div class="form-group">
+                        <label>Nama</label>
+                        <input type="hidden" name="id" id="id">
+                        <input type="text" name="nama" id="nama" class="form-control" required="true">
+                        <p class="text-danger" id="err_nama"></p>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-group">
+                        <label>Jenis Kelamin</label><br>
+                        <input type="radio" name="jenis_kelamin" id="jenkel1" value="L" required="true">Laki-laki
+                        <input type="radio" name="jenis_kelamin" id="jenkel2" value="P" required="true">Perempuan
+                    </div>
+                    <p class="text-danger" id="err_jenis_kelamin"></p>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label>Alamat</label>
+                <textarea name="alamat" id="alamat" class="form-control"></textarea>
+                <p class="text-danger" id="err_alamat"></p>
+            </div>
+
+            <div class="form-group">
+                <label>No Telepon</label>
+                <input type="number" name="no_telp" id="no_telp" class="form-control" required="true">
+                <p class="text-danger" id="err_no_telp"></p>
+            </div>
+
+            <div class="form-group">
+                <button type="button" name="simpan" id="simpan" class="btn btn-primary">
+                    <i class="fa fa-save"></i> Simpan
+                </button>
+            </div>
+        </form>
+
+        <hr>
+
+        <div class="data"></div>
+
+    </div>
+    <div class="text-center">&copy; <?= date('Y') ?> Copyright:
+        <a href="https://google.com/">Desain Dan Pemrograman Web</a>
+    </div>
+
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Datatable -->
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $.ajaxSetup({
+                headers: {
+                    'Csrf-Token': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $('.data').load("data.php");
+        });
+    </script>
+</body>
+</html>
