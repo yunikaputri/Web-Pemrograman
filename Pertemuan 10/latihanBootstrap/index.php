3d5041014beefbe4f3129fd02b95c1482e200ef3
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-23 23:41:51 +0700

    Menyisipkan Bootstrap

---
+++ Pertemuan 10/latihanBootstrap/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pengenalan Bootstrap</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+		<!-- Menyisipkan Bootstrap -->
+		<script src="https://ajay.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/pooper.js/1.16.0/umd/pooper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<h1>Selamat Datang di Website "Nabilah Rahmah"</h1>
+	<p>Saya sedang belajar Bootstrap</p>
+</body>
+</html>
