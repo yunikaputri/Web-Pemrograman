47d8e7459df1eb3db1d1b6aa252919ff18dc5cfc
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-03-31 23:35:48 +0700

    menghapus data anggota dari database berdasarkan ID yang diterima melalui parameter GET

--- Pertemuan 11/webprograming2024/fungsi/hapus.php
+++ Pertemuan 11/webprograming2024/fungsi/hapus.php
@@ -14,6 +14,21 @@ if(!empty($_SESSION['username'])){
             pesan('danger', "Jabatan Tidak Terhapus Karena: " .mysqli_error($koneksi));
         }
         header("Location: ../index.php?page=jabatan");
-    }
+    // Menghapus data anggota dari database berdasarkan ID yang diterima melalui parameter GET
+    } elseif (!empty($_GET['anggota'])) {
+        $id = antiinjection($koneksi, $_GET['id']);
+        $query = "DELETE FROM user WHERE id = '$id'";
+        if (mysqli_query($koneksi, $query)) {
+            $query2 = "DELETE FROM anggota WHERE user_id = '$id'";
+            if (mysqli_query($koneksi, $query2)) {
+                pesan('success', "Anggota Telah Terhapus.");
+            } else {
+                pesan('warning', "Data Login Terhapus Tetapi Data Anggota Tidak Terhapus Karena: " . mysqli_error($koneksi));
+            }
+        } else {
+            pesan('danger', "Anggota Tidak Terhapus Karena: " . mysqli_error($koneksi));
+        }
+        header("Location: ../index.php?page=anggota");
+    }  
 }
 ?>
