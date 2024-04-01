f7e83bd263c6c7c77ce0237c109593d8865ff3d2
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-04-01 11:04:06 +0700

    pemanggilan metode startEngine() tanpa pembuatan objek $car1

---
+++ Pertemuan 12/oop/oop.php
@@ -0,0 +1,21 @@
+<?php
+// Pemanggilan metode startEngine() tanpa pembuatan objek $car1
+class Car
+{
+    public $brand;
+
+    public function startEngine()
+    {
+        echo "Engine started!";
+    }
+}
+
+$car = new Car();
+$car -> brand = "Toyota";
+
+$car2 = new Car();
+$car2 -> brand = "Honda";
+
+$car1 -> startEngine();
+echo $car2 -> brand;
+?>
