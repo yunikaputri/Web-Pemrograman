e254ce541f65a0e1fce8c4379c6bcccd7270b06b
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-04-01 20:41:50 +0700

    menyembunyikan detail internal dan hanya mengekspos fungsionalitas yang diperlukan

--- Pertemuan 12/oop/shape.php
+++ Pertemuan 12/oop/shape.php
@@ -4,7 +4,7 @@ interface Shape {
     public function calculateArea();
 }
 
-class circle implements Shape {
+class Circle implements Shape {
     private $radius;
 
     public function __construct($radius)
@@ -34,14 +34,22 @@ class Rectangle implements Shape  {
     }
 }
 
-function printArea(Shape $shape)
-{
-    echo "Area: " . $shape -> calculateArea() . "<br>";
-}
+// function printArea(Shape $shape)
+// {
+//     echo "Area: " . $shape -> calculateArea() . "<br>";
+// }
 
+// $circle = new Circle(5);
+// $rectangle = new Rectangle(4, 6);
+
+// printArea($circle);
+// printArea($rectangle);
+
+
+// Menyembunyikan detail internal dan hanya mengekspos fungsionalitas yang diperlukan
 $circle = new Circle(5);
 $rectangle = new Rectangle(4, 6);
 
-printArea($circle);
-printArea($rectangle);
+echo "Area of Circle: " . $circle -> calculateArea() . "<br>";
+echo "Area of Rectangle: " . $rectangle -> calculateArea() . "<br>";
 ?>
