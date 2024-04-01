d044d2455258319bf5af9d07dc64fd8f4d793771
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-04-01 21:04:35 +0700

    mendefinisikan konstruktor untuk membuat pesan saat objek mobil dibuat, dan mengembalikan merek mobil

--- Pertemuan 12/oop/car.php
+++ Pertemuan 12/oop/car.php
@@ -1,37 +1,67 @@
 <?php 
-// Mendefinisikan atribut model dan color, serta menyediakan metode untuk mengakses dan mengubah nilai atribut
+// // Mendefinisikan atribut model dan color, serta menyediakan metode untuk mengakses dan mengubah nilai atribut
+// class Car
+// {
+//     private $model;
+//     private $color;
+
+//     public function __construct($model, $color)
+//     {
+//         $this -> model = $model;
+//         $this -> color = $color;
+//     }
+
+//     public function getModel()
+//     {
+//         return $this -> model;
+//     }
+
+//     public function setColor($color)
+//     {
+//         $this -> color = $color;
+//     }
+
+//     public function getColor()
+//     {
+//         return $this -> color;
+//     }
+// }
+
+// $car = new Car("Toyota", "Blue");
+
+// echo "Model: " . $car -> getModel() . "<br>";
+// echo "Color: " . $car -> getColor() . "<br>";
+
+// $car -> setColor("Red");
+
+// echo "Updated Color: " . $car -> getColor() . "<br>";
+
+
+
+// CONSTRUSTOR DAN DESTRUCTORS
+// Mendefinisikan konstruktor untuk membuat pesan saat objek mobil dibuat, dan mengembalikan merek mobil
 class Car
 {
-    private $model;
-    private $color;
+    private $brand;
 
-    public function __construct($model, $color)
+    public function __construct($brand)
     {
-        $this -> model = $model;
-        $this -> color = $color;
+        echo "A new car is created. <br>";
+        $this -> brand = $brand;
     }
 
-    public function getModel()
+    public function getBrand()
     {
-        return $this -> model;
+        return $this -> brand;
     }
 
-    public function setColor($color)
+    public function __destruct()
     {
-        $this -> color = $color;
-    }
-
-    public function getColor()
-    {
-        return $this -> color;
+        echo "The car is destroyed. <br>";   
     }
 }
 
-$car = new Car("Toyota", "Blue");
+$car = new Car("Toyota");
 
-echo "Model: " . $car -> getModel() . "<br>";
-echo "Color: " . $car -> getColor() . "<br>";
-
-$car -> setColor("Red");
-
-echo "Updated Color: " . $car -> getColor() . "<br>";
+echo "Brand: " . $car -> getBrand() . "<br>";
+?>
