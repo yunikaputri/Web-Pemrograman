e0557b38184a0825e9b4ec8203663208c95a12bc
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-04-01 22:05:25 +0700

    mengelola koneksi ke database MySQL serta menginisialisasi koneksi saat objek dibuat

---
+++ Pertemuan 12/oop/database.php
@@ -0,0 +1,19 @@
+<?php
+// Mengelola koneksi ke database MySQL serta menginisialisasi koneksi saat objek dibuat
+class Database
+{
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = "prakwebdb";
+    public $conn;
+
+    public function __construct()
+    {
+        $this->conn = new mysqli($this -> host, $this -> username, $this -> password, $this->database);
+
+        if ($this -> conn -> connect_error) {
+            die("connection failed: " . $this -> conn -> connect_error);
+        }
+    }
+}
